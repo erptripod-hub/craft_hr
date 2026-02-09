25efdee9cbd9cbb789e346109579c085febfb8ed
erptripod-hub <erp.tripod@gmail.com>
2026-02-09 14:26:40 +0400

    Update get_leaves.py

--- craft_hr/events/get_leaves.py
+++ craft_hr/events/get_leaves.py
@@ -4,10 +4,8 @@ def get_leaves(date_of_joining, allocation_start_date, leave_distribution_templa
     opening_months = round(frappe.utils.date_diff(allocation_start_date, date_of_joining) / 365 * 12)
     if opening_months < 0:
         frappe.throw("Leave Period from date should be after employee joining date")
-
     month_array = {}
     cumulative_allocation = {}
-
     template = frappe.get_doc('Leave Distribution Template', leave_distribution_template)
     for row in template.leave_distribution:
         if row.end != 0:
@@ -16,25 +14,19 @@ def get_leaves(date_of_joining, allocation_start_date, leave_distribution_templa
         else:
             month_array[row.start] = row.monthly_allocation
             month_array[row.end] = row.monthly_allocation
-
     allocation = 0
     for i in range(1, max(list(month_array.keys())) + 1):
         allocation += month_array[i]
         cumulative_allocation[i] = allocation
-
     cumulative_allocation[0] = month_array[0]
     max_months = max(list(cumulative_allocation.keys()))
-
     if opening_months <= max(list(month_array.keys())):
         leaves = cumulative_allocation[opening_months]
     else:
         leaves = cumulative_allocation[max_months] + cumulative_allocation[0] * (opening_months - max_months)
-
     if opening_months == 0:
         leaves = 0
-
     return leaves
-
 
 def get_earned_leave(employee=None):
     filters = {
@@ -42,23 +34,18 @@ def get_earned_leave(employee=None):
         'custom_leave_distribution_template': ['is', 'set'],
         'custom_status': "Ongoing"
     }
-
     if employee:
         filters['employee'] = employee
-
     for la in frappe.db.get_list('Leave Allocation', filters):
         doc = frappe.get_doc('Leave Allocation', la.name)
-
         to_date = frappe.utils.getdate()
         if doc.to_date < to_date:
             to_date = doc.to_date
-
         earned_leaves = get_leaves(
             doc.custom_date_of_joining,
             to_date,
             doc.custom_leave_distribution_template
         )
-
         new_used_leaves = frappe.db.count(
             'Attendance',
             {
@@ -68,12 +55,10 @@ def get_earned_leave(employee=None):
                 'attendance_date': ['between', [doc.from_date, doc.to_date]]
             }
         )
-
-       doc.new_leaves_allocated = round(earned_leaves - doc.custom_opening_used_leaves, 2)
+        doc.new_leaves_allocated = round(earned_leaves - doc.custom_opening_used_leaves, 2)
         doc.custom_used_leaves = round(doc.custom_opening_used_leaves + new_used_leaves, 2)
         doc.custom_available_leaves = round(doc.new_leaves_allocated - new_used_leaves, 2)
         
-        # Use db_set instead of save to bypass validation
         frappe.db.set_value('Leave Allocation', doc.name, {
             'new_leaves_allocated': doc.new_leaves_allocated,
             'custom_used_leaves': doc.custom_used_leaves,
